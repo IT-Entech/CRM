0542258bd4f09d4a01e6d3b671333f2513f1b73d
IT-Entech <IT-support@en-technology.com>
2024-10-22 16:16:59 +0700

    22-10

--- Admin/index-Maintanance.php
+++ Admin/index-Maintanance.php
@@ -231,7 +231,7 @@ if($staff === 0 || $level <= 1){
               <hr class="dropdown-divider">
             </li>
 
-            <li>
+            <!--li>
               <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
                 <i class="bi bi-person"></i>
                 <span>My Profile</span>
@@ -259,10 +259,10 @@ if($staff === 0 || $level <= 1){
             </li>
             <li>
               <hr class="dropdown-divider">
-            </li>
+            </li-->
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="/CRM/log_out.php">
+              <a class="dropdown-item d-flex align-items-center" href="https://entech-app.com/log_out.php">
                 <i class="bi bi-box-arrow-right"></i>
                 <span>Sign Out</span>
               </a>
@@ -279,256 +279,256 @@ if($staff === 0 || $level <= 1){
   <!-- ======= Sidebar ======= -->
   <aside id="sidebar" class="sidebar">
 
-    <ul class="sidebar-nav" id="sidebar-nav">
+<ul class="sidebar-nav" id="sidebar-nav">
 
-      <li class="nav-item">
-      <a class="nav-link collapsed" data-bs-target="#dashboards-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-grid"></i>
-          <span>Dashboard</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="dashboards-nav" class="nav-content show" data-bs-parent="#sidebar-nav">
-        <li>
-            <a href="index.php" >
-              <i class="bi bi-circle"></i><span>CRM</span>
-            </a>
-          </li>
-          <li>
-            <a href="index-Maintanance.php"class="active">
-              <i class="bi bi-circle"></i><span>Maintanance</span>
-            </a>
-          </li>
-        </ul>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-menu-button-wide"></i><span>Components</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="components-alerts.html">
-              <i class="bi bi-circle"></i><span>Alerts</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-accordion.html">
-              <i class="bi bi-circle"></i><span>Accordion</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-badges.html">
-              <i class="bi bi-circle"></i><span>Badges</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-breadcrumbs.html">
-              <i class="bi bi-circle"></i><span>Breadcrumbs</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-buttons.html">
-              <i class="bi bi-circle"></i><span>Buttons</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-cards.html">
-              <i class="bi bi-circle"></i><span>Cards</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-carousel.html">
-              <i class="bi bi-circle"></i><span>Carousel</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-list-group.html">
-              <i class="bi bi-circle"></i><span>List group</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-modal.html">
-              <i class="bi bi-circle"></i><span>Modal</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-tabs.html">
-              <i class="bi bi-circle"></i><span>Tabs</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-pagination.html">
-              <i class="bi bi-circle"></i><span>Pagination</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-progress.html">
-              <i class="bi bi-circle"></i><span>Progress</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-spinners.html">
-              <i class="bi bi-circle"></i><span>Spinners</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-tooltips.html">
-              <i class="bi bi-circle"></i><span>Tooltips</span>
-            </a>
-          </li>
-        </ul>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-journal-text"></i><span>Forms</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="forms-elements.html">
-              <i class="bi bi-circle"></i><span>Form Elements</span>
-            </a>
-          </li>
-          <li>
-            <a href="forms-appoint.php">
-              <i class="bi bi-circle"></i><span>บันทึกข้อมูลลูกค้า</span>
-            </a>
-          </li>
-          <li>
-            <a href="forms-order.php">
-              <i class="bi bi-circle"></i><span>บันทึก Order</span>
-            </a>
-          </li>
-          <li>
-            <a href="forms-validation.html">
-              <i class="bi bi-circle"></i><span>Form Validation</span>
-            </a>
-          </li>
-        </ul>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-layout-text-window-reverse"></i><span>Tables</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="tables-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="tables-general.html">
-              <i class="bi bi-circle"></i><span>General Tables</span>
-            </a>
-          </li>
-          <li>
-            <a href="tables-MA.php">
-              <i class="bi bi-circle"></i><span>Data Appoint</span>
-            </a>
-          </li>
-          <li>
-            <a href="tables-CS.html">
-              <i class="bi bi-circle"></i><span>Data Cost sheet</span>
-            </a>
-          </li>
-        </ul>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-bar-chart"></i><span>Charts</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="charts-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="charts-chartjs.html">
-              <i class="bi bi-circle"></i><span>Chart.js</span>
-            </a>
-          </li>
-          <li>
-            <a href="charts-apexcharts.html">
-              <i class="bi bi-circle"></i><span>ApexCharts</span>
-            </a>
-          </li>
-          <li>
-            <a href="charts-echarts.html">
-              <i class="bi bi-circle"></i><span>ECharts</span>
-            </a>
-          </li>
-        </ul>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-gem"></i><span>Icons</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="icons-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="icons-bootstrap.html">
-              <i class="bi bi-circle"></i><span>Bootstrap Icons</span>
-            </a>
-          </li>
-          <li>
-            <a href="icons-remix.html">
-              <i class="bi bi-circle"></i><span>Remix Icons</span>
-            </a>
-          </li>
-          <li>
-            <a href="icons-boxicons.html">
-              <i class="bi bi-circle"></i><span>Boxicons</span>
-            </a>
-          </li>
-        </ul>
-      </li>
-
-      <li class="nav-heading">Pages</li>
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="users-profile.html">
-          <i class="bi bi-person"></i>
-          <span>Profile</span>
+  <li class="nav-item">
+  <a class="nav-link collapsed" data-bs-target="#dashboards-nav" data-bs-toggle="collapse" href="#">
+      <i class="bi bi-grid"></i>
+      <span>Dashboard</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="dashboards-nav" class="nav-content show" data-bs-parent="#sidebar-nav">
+    <li>
+        <a href="index.php">
+          <i class="bi bi-circle"></i><span>CRM</span>
         </a>
       </li>
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="permission.php">
-          <i class="bi bi-question-circle"></i>
-          <span>Permission</span>
+      <li>
+        <a href="index-Maintanance.php" class="active">
+          <i class="bi bi-circle"></i><span>Maintanance</span>
         </a>
       </li>
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-contact.html">
-          <i class="bi bi-envelope"></i>
-          <span>Contact</span>
-        </a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-register.html">
-          <i class="bi bi-card-list"></i>
-          <span>Register</span>
-        </a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-login.html">
-          <i class="bi bi-box-arrow-in-right"></i>
-          <span>Login</span>
-        </a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-error-404.html">
-          <i class="bi bi-dash-circle"></i>
-          <span>Error 404</span>
-        </a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-blank.html">
-          <i class="bi bi-calendar-day"></i>
-          <span>Blank</span>
-        </a>
-      </li>
-
     </ul>
+  </li>
 
-  </aside>
+  <!--li class="nav-item">
+    <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+      <i class="bi bi-menu-button-wide"></i><span>Components</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+        <a href="components-alerts.html">
+          <i class="bi bi-circle"></i><span>Alerts</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-accordion.html">
+          <i class="bi bi-circle"></i><span>Accordion</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-badges.html">
+          <i class="bi bi-circle"></i><span>Badges</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-breadcrumbs.html">
+          <i class="bi bi-circle"></i><span>Breadcrumbs</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-buttons.html">
+          <i class="bi bi-circle"></i><span>Buttons</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-cards.html">
+          <i class="bi bi-circle"></i><span>Cards</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-carousel.html">
+          <i class="bi bi-circle"></i><span>Carousel</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-list-group.html">
+          <i class="bi bi-circle"></i><span>List group</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-modal.html">
+          <i class="bi bi-circle"></i><span>Modal</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-tabs.html">
+          <i class="bi bi-circle"></i><span>Tabs</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-pagination.html">
+          <i class="bi bi-circle"></i><span>Pagination</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-progress.html">
+          <i class="bi bi-circle"></i><span>Progress</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-spinners.html">
+          <i class="bi bi-circle"></i><span>Spinners</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-tooltips.html">
+          <i class="bi bi-circle"></i><span>Tooltips</span>
+        </a>
+      </li>
+    </ul>
+  </li-->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+      <i class="bi bi-journal-text"></i><span>Forms</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <!--li>
+        <a href="forms-elements.html">
+          <i class="bi bi-circle"></i><span>Form Elements</span>
+        </a>
+      </li-->
+      <li>
+        <a href="forms-appoint.php">
+          <i class="bi bi-circle"></i><span>บันทึกข้อมูลลูกค้า</span>
+        </a>
+      </li>
+      <li>
+        <a href="forms-order.php">
+          <i class="bi bi-circle"></i><span>บันทึก Order</span>
+        </a>
+      </li>
+      <!--li>
+        <a href="forms-validation.html">
+          <i class="bi bi-circle"></i><span>Form Validation</span>
+        </a>
+      </li-->
+    </ul>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
+      <i class="bi bi-layout-text-window-reverse"></i><span>Tables</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="tables-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+      <li>
+        <a href="tables-general.html">
+          <i class="bi bi-circle"></i><span>General Tables</span>
+        </a>
+      </li>
+      <li>
+        <a href="tables-data.php">
+          <i class="bi bi-circle"></i><span>Data Appoint</span>
+        </a>
+      </li>
+      <li>
+        <a href="tables-CS.html">
+          <i class="bi bi-circle"></i><span>Data Cost sheet</span>
+        </a>
+      </li>
+    </ul>
+  </li>
+
+  <!--li class="nav-item">
+    <a class="nav-link collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
+      <i class="bi bi-bar-chart"></i><span>Charts</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="charts-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+        <a href="charts-chartjs.html">
+          <i class="bi bi-circle"></i><span>Chart.js</span>
+        </a>
+      </li>
+      <li>
+        <a href="charts-apexcharts.html">
+          <i class="bi bi-circle"></i><span>ApexCharts</span>
+        </a>
+      </li>
+      <li>
+        <a href="charts-echarts.html">
+          <i class="bi bi-circle"></i><span>ECharts</span>
+        </a>
+      </li>
+    </ul>
+  </li-->
+
+  <!--li class="nav-item">
+    <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse" href="#">
+      <i class="bi bi-gem"></i><span>Icons</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="icons-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+        <a href="icons-bootstrap.html">
+          <i class="bi bi-circle"></i><span>Bootstrap Icons</span>
+        </a>
+      </li>
+      <li>
+        <a href="icons-remix.html">
+          <i class="bi bi-circle"></i><span>Remix Icons</span>
+        </a>
+      </li>
+      <li>
+        <a href="icons-boxicons.html">
+          <i class="bi bi-circle"></i><span>Boxicons</span>
+        </a>
+      </li>
+    </ul>
+  </li-->
+
+  <li class="nav-heading">Pages</li>
+
+  <!--li class="nav-item">
+    <a class="nav-link collapsed" href="users-profile.html">
+      <i class="bi bi-person"></i>
+      <span>Profile</span>
+    </a>
+  </li-->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="permission.php">
+      <i class="bi bi-question-circle"></i>
+      <span>Permission</span>
+    </a>
+  </li>
+
+  <!--li class="nav-item">
+    <a class="nav-link collapsed" href="pages-contact.html">
+      <i class="bi bi-envelope"></i>
+      <span>Contact</span>
+    </a>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-register.html">
+      <i class="bi bi-card-list"></i>
+      <span>Register</span>
+    </a>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-login.html">
+      <i class="bi bi-box-arrow-in-right"></i>
+      <span>Login</span>
+    </a>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-error-404.html">
+      <i class="bi bi-dash-circle"></i>
+      <span>Error 404</span>
+    </a>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-blank.html">
+      <i class="bi bi-calendar-day"></i>
+      <span>Blank</span>
+    </a>
+  </li-->
+
+</ul>
+
+</aside>
 
   <main id="main" class="main">
 

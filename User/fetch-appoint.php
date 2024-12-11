4f761d38f20c9d3d033a1cc82e857a85d4b71e39
IT-Entech <IT-support@en-technology.com>
2024-12-11 11:13:03 +0700

    update DB

--- User/fetch-appoint.php
+++ User/fetch-appoint.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-include_once '../../connectDB/connectWebDB.php';
+include_once '../../connectDB/connectDB.php';
 $objCon = connectDB(); // Connect to the database
 
 if ($objCon === false) {

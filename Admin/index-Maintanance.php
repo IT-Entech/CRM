9691ecbc136b28fda9312beebda99dedda267cff
IT-Entech <IT-support@en-technology.com>
2024-12-19 11:39:35 +0700

    uplogout

--- Admin/index-Maintanance.php
+++ Admin/index-Maintanance.php
@@ -264,7 +264,7 @@ if($staff === 0 || $level <= 1){
             </li-->
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="../log_out.php">
+              <a class="dropdown-item d-flex align-items-center" href="/log_out.php">
                 <i class="bi bi-box-arrow-right"></i>
                 <span>Sign Out</span>
               </a>
